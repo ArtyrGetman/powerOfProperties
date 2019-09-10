7afa38144d741ea2115c64fe11e823ad5f7ea851
art <artyrgetman@gmail.com>
2019-09-10 22:31:30 +0300

    update example

--- examples/src/main/java/com/properties/examples/examples/ExamplesApplication.java
+++ examples/src/main/java/com/properties/examples/examples/ExamplesApplication.java
@@ -3,6 +3,7 @@ package com.properties.examples.examples;
 import com.properties.examples.examples.strategy_example.MessageSender;
 import com.properties.examples.examples.strategy_example.properties_strategy_case.SenderProcessorByPropInitializer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@Log4j2
 public class ExamplesApplication implements CommandLineRunner {
 
     private final SenderProcessorByPropInitializer senderProcessorByPropInitializer;
@@ -23,10 +25,8 @@ public class ExamplesApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         System.out.println("\n Case 1. Base");
-        messageSenderMapMap.forEach((s, messageSender)
-                -> System.out.println(s + "/" + messageSender.doSomeCase()));
-        System.out.println("\n Case 2. Get senders from application.properties");
-        senderProcessorByPropInitializer.getSenderMap().forEach((s, messageSender)
-                -> System.out.println(s + "/" + messageSender.doSomeCase()));
+        System.out.println(messageSenderMapMap.get("sms").doSomeCase());
+        System.out.println("\n Case 1. Get sender bean from properties");
+        System.out.println(senderProcessorByPropInitializer.getSenderMap().get("push").doSomeCase());
     }
 }
